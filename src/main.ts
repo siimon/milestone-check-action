19c4140ef5bf3706b85acbbbe89c2bca7cc21c4d
Simon Nyberg <simon@nyberg.io>
2020-02-25 15:12:09 +0100

    Prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,38 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import { WebhookPayload } from '@actions/github/lib/interfaces';
+
+interface PullRequestWebhookPayload extends WebhookPayload {
+  // eslint-disable-next-line camelcase
+  pull_request?: {
+    [key: string]: any;
+    number: number;
+    // eslint-disable-next-line camelcase
+    html_url?: string;
+    body?: string;
+
+    milestone?: string;
+    base: {
+      ref: string;
+    },
+    head: {
+      ref: string;
+    },
+  },
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const pullRequest = (github.context.payload as PullRequestWebhookPayload).pull_request
+    if(!pullRequest) {
+      core.setFailed("Missing pull request data")
+      return
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if(!pullRequest.milestone) {
+      core.setFailed("Missing milestone in the pull request")
+      return
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
